dd5e3a0a7337337f23f708adeeb2e56e8823cd8c
Palpapeen <licks92@gmail.com>
2020-04-04 16:49:57 +0200

    Switch to styled component
    + a bit of cleanup

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const HeaderContainer = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
+  <Container>
+    <HeaderContainer>
       <p>En construction</p>
-    </header>
-  </div>
+    </HeaderContainer>
+  </Container>
 );
 
 export default App;
