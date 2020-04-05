666c9ecf57db973ad57b2075629f983fd9c19ccb
Erwan de Lépinau <erwan@feedly.com>
2020-04-05 16:02:27 -0700

    add image utilities

--- back-app/research/form_vision/main.py
+++ back-app/research/form_vision/main.py
@@ -1,5 +1,5 @@
 from research.form_vision.image import Image
-from research.form_vision.processor import ImageProcessor
+from research.form_vision.template_matching import TemplateMatcher
 from pathlib import Path
 
 if __name__ == "__main__":
@@ -7,11 +7,14 @@ if __name__ == "__main__":
     DIRNAME = Path(__file__).parent
 
     path = f"{DIRNAME}/../../../resources/germaine.png"
+    template_path = f"{DIRNAME}/../../../resources/thierry.png"
+    template = Image.from_file(template_path, "template")
+    matcher = TemplateMatcher(template, ((0.044, 0.28), (0.07, 0.72)))
+
     img = Image.from_file(path)
-    proc = ImageProcessor(img)
+    matcher.match(img)
+
     coords = {
-        "lefty": ((0.35, 0.1), (0.4, 0.9)),
-        "righty": ((0.4, 0.1), (0.45, 0.9)),
+        "voyage hors de france": ((0.2, 0.56), (0.24, 0.7)),
+        "tickboxes1": ((0.365, 0.785), (0.643, 0.885)),
     }
-    for img in proc.select_subimages(coords).values():
-        img.show()
