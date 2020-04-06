25b3e486670f00af6b7f85dd3e9a9b9d65c1fefa
DeciusAntiochus <awaidaantoine@yahoo.fr>
2020-04-06 10:48:38 +0200

    modif port test

--- back-app/test.py
+++ back-app/test.py
@@ -28,5 +28,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=3000, debug=True, threaded=True)
 
